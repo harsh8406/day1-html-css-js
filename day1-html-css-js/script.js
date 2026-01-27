fdba30cbfaf60f4b93b9ff1435d3e8bb8ed648e5
harsh8406 <premgaikwad1873@gmail.com>
2026-01-27 15:05:31 +0530

    Add Day 1 HTML CSS JS project

---
+++ day1-html-css-js/script.js
@@ -0,0 +1,11 @@
+console.log("Welcome to JavaScript!"); 
+function greet(name) { 
+    console.log("Hello " + name); 
+} 
+greet("Anmol"); 
+let tasks = ["HTML", "CSS", "JavaScript"]; 
+for (let i = 0; i < tasks.length; i++) { 
+    console.log("Task:", tasks[i]); 
+} 
+
+
